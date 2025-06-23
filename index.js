79a29b9be3c994e86137e33ea64390d65ddbf9f5
Nur Faidzin <nurfaidzin@student.janabadra.ac.id>
2025-06-23 22:25:50 +0700

    Setup arkana

---
+++ index.js
@@ -0,0 +1,32 @@
+// import express
+const express = require('express')
+
+// import cors
+const cors = require('cors')
+
+// import body parser
+const bodyParser = require('body-parser')
+
+// init app
+const app = express()
+
+// use cors
+app.use(cors())
+
+// use body parser
+app.use(bodyParser.urlencoded({extended:false}))
+
+// parse to json
+app.use(bodyParser.json())
+
+// define port
+const port = 3000; 
+
+app.get('/', (req, res) => {
+    res.send('Hello world')
+})
+
+// start the server
+app.listen(port, () => {
+    console.log(`Server started in port ${port}`)
+})
