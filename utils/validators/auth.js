bb2db0450d8945b09694676e3411ab6052e558dd
Nur Faidzin <nurfaidzin@student.janabadra.ac.id>
2025-06-24 21:10:12 +0700

    add validator for auth and user create

---
+++ utils/validators/auth.js
@@ -0,0 +1,31 @@
+const { body } = require('express-validator')
+const { prisma } = require('../../prisma/client');
+
+// Define validation for register
+const validateRegister = [
+    body('name').notEmpty().withMessage('Name is required'),
+    body('email')
+        .notEmpty().withMessage('Email is required')
+        .isEmail().withMessage('Email is invalid')
+        .custom(async (value) => {
+            if (!value) {
+                throw new Error('Email is required');
+            }
+
+            const user = await prisma.user.findUnique({where: {email: value}});
+            if (user) {
+                throw new Error('Email already exists');
+            }
+
+            return true
+        }),
+    body('password').isLength({min: 6}).withMessage('Password must be at least 6 characters'),
+];
+
+// Define validation for login
+const validateLogin = [
+    body('email').notEmpty().withMessage('Email is required'),
+    body('password').isLength({min: 6}).withMessage('Password must be at least 6 characters'),
+];
+
+module.exports = {validateLogin, validateRegister}
