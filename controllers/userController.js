f0e6ae38f64b0ea17de21d71f54c70b5fb73c2eb
Nur Faidzin <nurfaidzin@student.janabadra.ac.id>
2025-06-29 08:30:29 +0700

    add user crud process

--- controllers/userController.js
+++ controllers/userController.js
@@ -72,4 +72,105 @@ const createUser = async (req, res) => {
     }
 }
 
-module.exports = { findUsers, createUser };
+// function find user by id
+const findUserById = async (req, res) => {
+    const { id } = req.params;
+
+    try {
+        // get user by id
+        const user = await prisma.user.findUnique({
+            where: {
+                id: Number(id)
+            },
+            select:{
+                id: true,
+                name: true,
+                email: true
+            }
+        });
+
+        res.status(200).send({
+            success: true,
+            message: `Get user by Id :${id}`,
+            data: user,
+        });
+
+    } catch (error) {
+        res.status(500).send({
+            success: true,
+            message: "Internal server error",
+        });
+    }
+};
+
+// function update user
+const updateUser = async (req, res) => {
+    const { id } = req.params;
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            success: false,
+            message: "Validation error",
+            errors: errors.array()
+        });
+    }
+
+    let hashedPassword = undefined;
+    if (req.body.password && req.body.password.trim() !== "") {
+        hashedPassword = await bcrypt.hash(req.body.password, 10);
+    }
+
+    try {
+        const user = await prisma.user.update({
+            where: {
+                id: Number(id),
+            },
+            data: {
+                name: req.body.name,
+                email: req.body.email,
+                password: hashedPassword
+            },
+        });
+
+        return res.status(402).send({
+            success: true,
+            message: "Update user successfully",
+            data: user
+        })
+    } catch (error) {
+        return res.status(500).send({
+            success: false,
+            message: "Internal server error",
+        })
+    }
+}
+
+// function delete user
+const deleteUser = async (req, res) => {
+    // get id form params
+    const { id } = req.params;
+
+    try {
+        await prisma.user.delete({
+            where: {
+                id: Number(id)
+            },
+        });
+
+        res.status(200).send({
+            success: true,
+            message: "User deleted successfully"
+        });
+
+
+    } catch (error) {
+        return res.status(500).send({
+            success: false,
+            message: "Internal server error"
+        });
+    }
+}
+
+module.exports = { findUsers, createUser, findUserById, updateUser, deleteUser };
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const verifyToken = require("../middlewares/auth")
 const registerController = require("../controllers/RegisterController");
 const loginController = require("../controllers/LoginController")
-const userController = require("../controllers/userController")
+const userController = require("../controllers/UserController")
 const { validateRegister, validateLogin } = require("../utils/validators/auth");    
 
 // define route
@@ -12,5 +12,8 @@ router.post('/login', validateLogin, loginController.login)
 
 router.get('/admin/users', verifyToken, userController.findUsers);
 router.post('/admin/users', verifyToken, userController.createUser);
+router.get('/admin/users/:id', verifyToken, userController.findUserById);
+router.put("/admin/users/:id", verifyToken, userController.updateUser)
+router.delete("/admin/users/:id", verifyToken, userController.deleteUser)
 
 module.exports = router
